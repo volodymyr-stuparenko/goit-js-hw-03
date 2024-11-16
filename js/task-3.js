c2df6ba0d513ae9cfba339ae9f49f27b87141756
Volodymyr <StuparenkoVladimir@gmail.com>
2024-11-16 15:25:04 +0200

    hw js 03 update

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,18 @@
-function checkForSpam(message) {
-    const normMessage = message.toLowerCase();
+function filterArray(numbers, value) {
+  const filterNumbers = [];
 
-    if (normMessage.includes('spam') || normMessage.includes('sale')) {
-        return true;
-    } else {
-        return false;
+  for (const number of numbers) { 
+    if (number > value) {
+      filterNumbers.push(number);
     }
+  }
+
+  return filterNumbers;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
